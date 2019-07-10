56cc63e1fc20b108e79ddafb5eb679e2f7538b0d
stackfing <stackfing@gmail.com>
2019-07-10 23:04:34 +0800

    init

---
+++ sform-core/src/test/java/com/stackfing/sform/SformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackfing.sform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
