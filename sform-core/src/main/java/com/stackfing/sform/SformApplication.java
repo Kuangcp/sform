0870364247cda9a6a7d496214b88ce2db7bc575a
stackfing <stackfing@gmail.com>
2019-07-10 23:10:32 +0800

    mybatis-plust 配置

--- sform-core/src/main/java/com/stackfing/sform/SformApplication.java
+++ sform-core/src/main/java/com/stackfing/sform/SformApplication.java
@@ -1,9 +1,11 @@
 package com.stackfing.sform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.stackfing.sform.mapper")
 public class SformApplication {
 
 	public static void main(String[] args) {
