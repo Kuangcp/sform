74dc5557bf60e1a8cbf6c8a7e399ee698818e5d7
stackfing <stackfing@gmail.com>
2019-07-20 22:31:50 +0800

    新增表异常

---
+++ sform-core/src/main/java/com/stackfing/sform/exception/TableException.java
@@ -0,0 +1,13 @@
+package com.stackfing.sform.exception;
+
+/**
+ * @Author: fing
+ * @Description:
+ * @Date: 下午10:13 19-7-20
+ * @Since:
+ */
+public class TableException extends SformException {
+	public TableException(String message) {
+		super(message);
+	}
+}
