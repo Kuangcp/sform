6fade59f1cbadbd49d3c37d4f078ba1bef882ca1
stackfing <stackfing@gmail.com>
2019-07-20 21:02:06 +0800

    自定义异常

---
+++ sform-core/src/main/java/com/stackfing/sform/exception/SformException.java
@@ -0,0 +1,12 @@
+package com.stackfing.sform.exception;
+
+/**
+ * @Author: fing
+ * @Description:
+ * @Date: 下午8:51 19-7-20
+ */
+public class SformException extends RuntimeException {
+	public SformException(String message) {
+		super(message);
+	}
+}
