034894a24775d9a07381a6c932754784dcfa778e
stackfing <stackfing@gmail.com>
2019-07-23 22:23:32 +0800

    尝试添加starter

--- sform-core/src/main/java/com/stackfing/sform/base/support/DefaultTableCreater.java
+++ sform-core/src/main/java/com/stackfing/sform/base/support/DefaultTableCreater.java
@@ -4,15 +4,12 @@ import com.stackfing.sform.base.AbstractTableCreater;
 import com.stackfing.sform.base.Table;
 import com.stackfing.sform.mapper.TablesMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 /**
  * @Author: fing
  * @Description:
  * @Date: 下午5:54 19-7-20
  */
-
-@Component
 public class DefaultTableCreater extends AbstractTableCreater {
 
 	@Autowired
