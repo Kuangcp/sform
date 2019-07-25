5e86950fa13dfbaf9e204dc29112e0a2f10f014b
stackfing <stackfing@gmail.com>
2019-07-25 22:42:47 +0800

    生成表单

---
+++ sform-front/src/components/WidgetItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-form-item>
+    <template v-if="widget.type == 'input'">
+      <el-input
+        :disabled="widget.disable"
+        v-model="widget.defaultValue"
+      >{{widget.name}}</el-input>
+    </template>
+    <template v-if="widget.type == 'button'">
+      <el-button :disabled="widget.disable">{{widget.defaultValue}}</el-button>
+    </template>
+    <template v-if="widget.type == 'select'">
+      <el-select
+        v-model="widget.defaultValue"
+        :placeholder="widget.placeholder"
+      >
+        <el-option
+          v-for="i in (widget.options.remote==true) ? widget.options.remoteOption : widget.options.option"
+          :key='i.value'
+          :value='i.value'
+          :label='i.label'
+        >
+        </el-option>
+        <!-- <el-option v-for="i in [{value: '1', label: '123123'}]" value="value">
+        </el-option> -->
+        <!-- <el-option
+            v-for="(i,index) in (item.options.remote == true ? [{label: "远程数据",value: "1"}]:i.options.option)"
+            :key="index"
+            :label="i.options.label"
+            :value="i.options.value"
+          >
+          </el-option> -->
+      </el-select>
+    </template>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  props: ["widget"],
+  data() {
+    return {
+        
+    };
+  },
+  created() {
+      console.log('asdfdsf')
+      this.axios.get(this.widget.options.remoteUrl).then(res=>{
+          this.widget.options.remoteOption = res.data
+      })
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ sform-front/src/views/Generate.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <router-link to="/">首页</router-link>
+    <el-form>
+      <template v-for="item in json.list">
+        <widgetitem :widget="item" />
+      </template>
+    </el-form>
+    <el-button @click="getJson">获取json</el-button>
+  </div>
+</template>
+
+<script>
+import widgetitem from "@/components/WidgetItem";
+export default {
+  components: { widgetitem },
+  data() {
+    return {
+      json: {
+        list: [
+          {
+            type: "select",
+            name: "sex",
+            defaultValue: "",
+            disable: true,
+            placeholder: "请选择数据哦",
+            options: {
+              remote: true,
+              remoteUrl: "http://localhost:8080/getAllOptions",
+              label: "label",
+              value: "value",
+              remoteOption: [],
+              option: [
+                {
+                  label: "选项二",
+                  value: "2"
+                },
+                {
+                  label: "选项一",
+                  value: "1"
+                }
+              ]
+            }
+          },
+          {
+            type: "input",
+            name: "age",
+            defaultValue: "",
+            disable: false,
+            placeholder: "",
+            options: {}
+          },
+          {
+            type: "button",
+            name: "按钮",
+            defaultValue: "asdd",
+            disable: true,
+            placeholder: "",
+            post: false,
+            options: {}
+          }
+        ],
+        config: {
+          labelWidth: 100,
+          labelPosition: "right",
+          size: "small",
+          customClass: ""
+        }
+      },
+      values: {}
+    };
+  },
+  methods: {
+    handler() {
+      console.log(this.list);
+    },
+    getJson() {
+        var that = this;
+        console.log(this.json.list)
+        for(var i =0;i<this.json.list.length;i++) {
+            var lst = this.json.list[i]
+            var obj = {}
+            if(lst.post == false) {
+                continue;
+            }
+            this.values[lst.name] = lst.defaultValue
+        }
+        
+        console.log(this.values)
+        this.$alert(this.values, '提交的json数据', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.values = {};
+          }
+        });
+    }
+  },
+  created() {},
+  mounted() {
+  }
+};
+</script>
+
+<style scoped>
+</style>
